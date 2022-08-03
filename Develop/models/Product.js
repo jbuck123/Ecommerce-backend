abdf38e0b9702f03103f21cd6a43a51390631832
JamesB <James.buchmann@gmail.com>
2022-08-02 19:05:48 -0500

    created Product model

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,23 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+ 
+    product_name : {
+      type: dataTypes.STRING,
+      defaultValues: null,
+    },
+    price : {
+      type: dataTypes.INTEGER,
+      defaultValues: null,
+    },
+    stock : {
+      type: dataTypes.INTEGER,
+      defaultValues:null
+    },
+    category_id: {
+      type: dataTypes.INTEGER,
+      defaultValues: null,
+    }
   },
   {
     sequelize,
